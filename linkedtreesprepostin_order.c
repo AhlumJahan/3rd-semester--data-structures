727f3e67b473bcf12372bb909235f3593fceaf7c
AhlumJahan <ahlumjahan@gmail.com>
2025-12-27 08:21:42 +0530

    traversal in trees

---
+++ linkedtreesprepostin_order.c
@@ -0,0 +1,56 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node{
+    int data;
+    struct node *right;
+    struct node *left;
+};
+struct node * createnode(int data){
+    struct node *n=(struct node *)malloc(sizeof(struct node));
+    n->data=data;
+    n->left=NULL;
+    n->right=NULL;
+    return n;}
+void preorder(struct node *root){
+    if(root!=NULL){
+        printf("%d",root->data);
+        preorder(root->left);
+        preorder(root->right);
+    }
+}
+    void postorder(struct node *root){
+    if(root!=NULL){
+        postorder(root->left);
+        postorder(root->right);
+        printf("%d",root->data);
+
+    }
+}
+void inorder(struct node *root){
+    if(root!=NULL){
+        inorder(root->left);
+        printf("%d",root->data);
+        inorder(root->right);
+    }
+}
+
+int main(){
+   struct node *p =createnode(6);
+    struct node *p1=createnode(7);
+    struct node *p2=createnode(9);
+    struct node *p3=createnode(2);
+    struct node *p4=createnode(1);
+
+    p->left=p1;
+    p->right=p2;
+    p1->left=p3;
+    p1->right=p4;
+preorder(p);
+printf("\n");
+postorder(p);
+printf("\n");
+
+inorder(p);
+return 0;
+
+}
